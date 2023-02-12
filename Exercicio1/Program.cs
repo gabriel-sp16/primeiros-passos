369af6f13a82af41ebf122e714b010a6f7eb0ba8
Gabriel De Souza Almeida <gabrielsouzaalmeida07@gmail.com>
2023-02-11 22:49:30 -0300

    atividades do curso de c#,atividade 1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int valor1 = 90;
+            int valor2 = 30;
+            int resultado = valor1 + valor2;
+
+            Console.WriteLine("soma ="+ resultado);
+
+
+
+
+
+        }
+    }
+}
